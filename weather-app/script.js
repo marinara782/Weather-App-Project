bded83664be60b50caac784cbe4c462f63648d7c
30237507 <marena.matavao@ucalgary.ca>
2025-05-21 17:08:11 -0600

    Touched up on the CSS part of my code. Began working on the JS part of code by starting fetchWeather().

---
+++ weather-app/script.js
@@ -0,0 +1,11 @@
+/*
+* Writing the fetchWeather() function so that when you click
+* it should fetch the weather
+*/
+
+function fetchWeather() {
+    let searchInput = document.getElementById("search").value;
+    const weatherDataSection = document.getElementById("weather-data");
+    weatherDataSection.style.display = "block";
+    const apiKey = ""; /* replaced with empty string to avoid inappropriate use. */
+}
