8fbd2156da199b252d7fd595b94051d6038c7776
30237507 <marena.matavao@ucalgary.ca>
2025-05-21 18:42:20 -0600

    Finished working on getLonAndLat() function. Now currently working on Displaying weather data!

--- weather-app/script.js
+++ weather-app/script.js
@@ -27,10 +27,45 @@ function fetchWeather() {
     async function getLonAndLat() {
         const countryCode = 1;
         const geoCodeURL = 'https://api.openweathermap.org/geo/1.0/direct?q=${searchInput.replace(" ", "%20")},${countryCode}&limit=1&appid=${apiKey}';
+        const response = await fetch(geoCodeURL);
+        if (!response.ok) {
+            console.log("Bad response! ", response.status);
+            return;
+        }
+
+        const data = await response.json();
+
+        if (data.length == 0) {
+        console.log("Something went wrong here.");
+        weatherDataSection.innerHTML = '
+        <div>
+            <h2>Invalid Input: "${searchInput}"</h2>
+            <p>Please try again with a valid <u>city name</u>.</p>
+        </div>
+        ';
+        return;
+        } else {
+            return data[0];
+        }
     }
 
     async function getWeatherData(lon, lat) {
+        const weatherURL = 'https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}';
 
+        /*
+        * Define a response variable and assign it to the object returned by the fetch() function.
+        */
+        const response = await fetch(weatherURL);
+        if(!response.ok) {
+        console.log("Bad Response! ", response.status);
+        return;
+        }
+
+        const data = await response.json();
+
+        /*
+        * The above statement should print an error message and return nothing if there is a problem.
+        */
     }
     }
 }
