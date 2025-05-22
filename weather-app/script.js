18dc375081767d57543e2985a5057a6c9d97cba3
30237507 <marena.matavao@ucalgary.ca>
2025-05-21 18:04:12 -0600

    Finished working on first part of fetchWeather function as well as get lon and lat, currently working on get weather data!

--- weather-app/script.js
+++ weather-app/script.js
@@ -8,4 +8,29 @@ function fetchWeather() {
     const weatherDataSection = document.getElementById("weather-data");
     weatherDataSection.style.display = "block";
     const apiKey = ""; /* replaced with empty string to avoid inappropriate use. */
+
+    /*
+    * if statement that displays a custom message for empty input.
+    */
+    if (searchInput = "") {
+    weatherDataSection.innerHTML = '
+    <div>
+        <h2>Empty Input!</h2>
+        <p>Please try again with a valid <u> city name</u>.</p>
+    </div>
+    ';
+    return;
+
+    /*
+    * uses the OpenWeather's API to return longitude and latitude data based on search input.
+    */
+    async function getLonAndLat() {
+        const countryCode = 1;
+        const geoCodeURL = 'https://api.openweathermap.org/geo/1.0/direct?q=${searchInput.replace(" ", "%20")},${countryCode}&limit=1&appid=${apiKey}';
+    }
+
+    async function getWeatherData(lon, lat) {
+
+    }
+    }
 }
